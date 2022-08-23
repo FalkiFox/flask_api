cded3fc54b5894c679c82593141a81cb19f5a64c
FalkiFox <juwol1423@gmail.com>
2022-08-23 21:32:18 +0200

    Added reserveTable Endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Modules
+# Modules / Functions
 from flask import Flask, request
-from functions import db_getfreetables, db_getalltables
+from functions import db_getfreetables, db_getalltables, db_reservetable
 
 # Create instance of the Flask class.
 app = Flask(__name__)
@@ -16,6 +16,12 @@ def getalltable():
 def getfreetable():
     params = request.get_json()
     return db_getfreetables(date=params["date"], time=params["time"])
+
+
+@app.route("/api/reserveTable/", methods=["POST"])
+def reservetable():
+    params = request.get_json()
+    return db_reservetable(date=params["date"], time=params["time"], tableid=params["tableID"])
 
 
 # Main
