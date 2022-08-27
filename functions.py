595259a79269d9343cabd22043a093d1b9202c82
FalkiFox <juwol1423@gmail.com>
2022-08-27 19:37:11 +0200

    Added new functions / overworked existing ones
    + db_executequery
    ~ service_reservetable
    + service_canceltable
    + service_getreservations

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 # Modules
-import random
 import sqlite3
-from flask import jsonify
+import random
 
 
 # Functions
@@ -12,80 +11,93 @@ def dict_factory(cursor, row):
     return d
 
 
-def db_getalltables():
+def db_executequery(query, mode):
     conn = sqlite3.connect("buchungssystem.sqlite")
     conn.row_factory = dict_factory
-    print(f"Established connection to database")
-    print(f"Getting all tables")
     cursor = conn.cursor()
-    cursor.execute("SELECT tischnummer, anzahlPlaetze FROM tische")
-    return jsonify(cursor.fetchall())
+    cursor.execute(query)
+    if mode == "insert" or mode == "update":
+        conn.commit()
+    else:
+        return cursor.fetchall()
 
 
-def db_getlastreservationnumber():
-    conn = sqlite3.connect("buchungssystem.sqlite")
-    conn.row_factory = dict_factory
-    print(f"Established connection to database")
-    print(f"Getting last reservation")
-    cursor = conn.cursor()
-    cursor.execute("SELECT max(reservierungsnummer) FROM reservierungen")
-    return jsonify(cursor.fetchall())
+def service_getalltables():
+    return db_executequery("SELECT tischnummer, anzahlPlaetze FROM tische", mode="select")
 
 
-def db_getallreservationsontimestamp(date, time):
-    conn = sqlite3.connect("buchungssystem.sqlite")
-    conn.row_factory = dict_factory
-    print(f"Established connection to database")
-    print(f"Getting all reservations on {date} {time}")
-    cursor = conn.cursor()
+def service_getreservations(date):
+    return db_executequery(f"SELECT reservierungsnummer,tischnummer,zeitpunkt FROM reservierungen WHERE zeitpunkt LIKE '{date}%'", mode="select")
+
+
+def service_getlastreservationnumber():
+    queryresult = db_executequery("SELECT max(reservierungsnummer) AS max_reservsnr FROM reservierungen", mode="select")
+    return queryresult
+
+
+def service_getallreservationsontimestamp(date, time):
     query = f"""
-    SELECT tische.tischnummer, tische.anzahlPlaetze, reservierungen.zeitpunkt
+    SELECT tische.tischnummer, tische.anzahlPlaetze, reservierungen.reservierungsnummer, reservierungen.zeitpunkt, reservierungen.storniert
     FROM tische
     LEFT JOIN reservierungen ON tische.tischnummer = reservierungen.tischnummer
     WHERE reservierungen.zeitpunkt = '{date} {time}'
     """
-    cursor.execute(query)
-    return jsonify(cursor.fetchall())
+    queryresult = db_executequery(query, mode="select")
+    return queryresult
 
 
-def db_getfreetables(date, time):
-    allTables = db_getalltables().get_json()
-    allReservations = db_getallreservationsontimestamp(date, time).get_json()
-    print(f"Getting free tables on {date} {time}")
+def service_getfreetables(date, time):
+    allTables = service_getalltables()
+    allReservations = service_getallreservationsontimestamp(date, time)
     free_tables = []
     for table in allTables:
         reserved = False
         for reservation in allReservations:
-            if table["tischnummer"] == reservation["tischnummer"]:
+            if table["tischnummer"] == reservation["tischnummer"] and reservation["storniert"] == "False":
                 reserved = True
         if not reserved:
             free_tables.append(table)
-    returnDict = {
-        "tag": date,
-        "uhrzeit": time,
-        "tables": free_tables
-    }
-    return returnDict
+    return free_tables
 
 
-def db_reservetable(date, time, tableid):
-    allReservations = db_getallreservationsontimestamp(date, time).get_json()
-    lastReservation = db_getlastreservationnumber().get_json()
-
-    exit()
-    for reservation in allReservations:
-        if reservation["tischnummer"] == tableid:  # Es gibt zu dem Zeitpunkt bereits eine Reservierung mit der Tisch ID
-            print("Error: Tisch zu dieser Zeit bereits belegt.")
-    conn = sqlite3.connect("buchungssystem.sqlite")
-    conn.row_factory = dict_factory
-    print(f"Established connection to database")
-    print(f"Creating reservation on {date} {time} for table {tableid}")
-    cursor = conn.cursor()
+def service_reservetable(date, time, tableid):
+    allReservations = service_getallreservationsontimestamp(date, time)
+    nextReservation = service_getlastreservationnumber()[0]["max_reservsnr"] + 1
+    reserved = False
+    stornopin = random.randint(1000, 9999)
     query = f"""
-    INSERT INTO reservierungen
-    (reservierungsnummer,zeitpunkt,tischnummer,pin,storniert)
-    VALUES ('11','{date} {time}',{tableid},{random.randint(1000,9999)},'False')
-    """  # TODO: Reservierungsnummer entsprechend setzen.
-    cursor.execute(query)
-    conn.commit()
-    return "Success"
+        INSERT INTO reservierungen
+        (reservierungsnummer,zeitpunkt,tischnummer,pin,storniert)
+        VALUES ('{nextReservation}','{date} {time}',{tableid}, {stornopin},'False')
+        """
+    for reservation in allReservations:
+        if reservation["tischnummer"] == int(tableid):
+            reserved = True
+    if not reserved:
+        db_executequery(query, mode="insert")
+        return {
+            "status": "Success",
+            "tag": date,
+            "uhrzeit": time,
+            "tischnummer": tableid,
+            "reservation": nextReservation,
+            "stornierungspin": stornopin
+        }
+    else:
+        return service_getfreetables(date=date, time=time)
+
+
+def service_canceltable(reservierungsnummer, pin):
+    reservationToCancel = db_executequery(f"SELECT * FROM reservierungen WHERE reservierungsnummer == {reservierungsnummer} AND pin == {pin}", mode="select")
+    if reservationToCancel[0]["storniert"] == "False":
+        db_executequery(f"UPDATE reservierungen SET storniert='True' WHERE pin={pin}", mode="update")
+        return {
+            "status": "Success",
+            "timestamp": reservationToCancel[0]["zeitpunkt"],
+            "tischnummer": reservationToCancel[0]["tischnummer"],
+            "reservation": reservationToCancel[0]["reservierungsnummer"]
+        }
+    else:
+        return {
+            "status": "Error: Reservation already cancelled"
+        }
